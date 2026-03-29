d05746ac083608c9d54d976ce8c4eaac3beb38df
KH-Coder865 <kaushikharsha2020@gmail.com>
2026-03-29 22:51:48 +0530

    Added a simple login page to the app

--- frontend/CivicFixApp/App.jsx
+++ frontend/CivicFixApp/App.jsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={require('./assets/favicon.png')} />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View> */}
+      <Login />
+    </>
   );
 }
 
